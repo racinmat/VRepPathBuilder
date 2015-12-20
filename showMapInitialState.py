7e5dfbe9bc9813c1b9f9181d971c2be72ef0e279
Azathoth <memnarch@seznam.cz>
2015-12-20 17:03:39 +0100

    přidán nový json, ve kterém je i definice mapy přidán skript, který přepočítá souřadnice programu do souřadnic vrepu, abych mohl ručně nastavit mapu pro simulaci

--- showMapInitialState.py
+++ showMapInitialState.py
@@ -1,8 +1,5 @@
-import vrep
-import sys
 import json
 import pprint
-import time
 
 def resizeCoords(data, oldSize, newSize):
 	ratio = newSize / oldSize
@@ -104,53 +101,3 @@ print('goals')
 pprint.pprint(data['map']['goals'])
 print('uav start')
 pprint.pprint(data['path'][0])	# uav start
-# print(len(obstacles))
-# pprint.pprint(uavCount)
-# for i in range(uavCount):
-#     pprint.pprint('Quadricopter_target#' + str(i - 1))
-
-vrep.simxFinish(-1)  # just in case, close all opened connections
-clientID=vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)  # Connect to V-REP
-if clientID == -1:
-    sys.exit(1)
-
-
-# vytvoření překážek
-_, baseCuboid = vrep.simxGetObjectHandle(clientID, 'Cuboid', vrep.simx_opmode_oneshot_wait)
-
-for obstacle in obstacles:
-	vrep.shape
-	_, [obstacleCuboid] = vrep.simxCopyPasteObjects(clientID, [baseCuboid], vrep.simx_opmode_oneshot_wait)
-	vrep.object
-	time.sleep(.5)	# při copypaste od sebe objekty odletí, počkám, než se "uklidní"
-	# vrep.simxSetObjectPosition(clientID, obstacleCuboid, -1, [obstacle['location']['x'], obstacle['location']['y'], 0], vrep.simx_opmode_oneshot_wait)
-	for i in range(1, 5):
-		vrep.simxSetObjectPosition(clientID, obstacleCuboid, -1, [-0.995, -0.995, 0], vrep.simx_opmode_oneshot_wait)
-
-# konec vytvoření překážek
-
-x = 0.5
-y = 0.5
-z = 0.5
-
-# _, temp = vrep.simxCreateDummy(clientID, 0.1, None, vrep.simx_opmode_oneshot_wait)
-# vrep.simxSetObjectPosition(clientID, temp, -1, [x, y, z], vrep.simx_opmode_oneshot_wait) # -1 je ku celé scéně, v tempu je handle, v podrtžítku návratová hodnota, oneshot wait čeká, než se příkaz provede
-
-# vezmu handle targetů kvadrokoptér
-
-targets = [0] * uavCount  # 2 je počet kvadrokoptér
-uavs = [0] * uavCount  # 2 je počet kvadrokoptér
-
-_, targets[0] = vrep.simxGetObjectHandle(clientID, 'Quadricopter_target', vrep.simx_opmode_oneshot_wait)
-_, uavs[0] = vrep.simxGetObjectHandle(clientID, 'Quadricopter', vrep.simx_opmode_oneshot_wait)
-
-for i in range(uavCount):
-    _, targets[i] = vrep.simxGetObjectHandle(clientID, 'Quadricopter_target#' + str(i - 1), vrep.simx_opmode_oneshot_wait)
-    _, uavs[i] = vrep.simxGetObjectHandle(clientID, 'Quadricopter#' + str(i - 1), vrep.simx_opmode_oneshot_wait)
-
-
-# vrep.simxSetObjectPosition(clientID, targets[0], -1, [x, y, z], vrep.simx_opmode_oneshot_wait)  # používat streaming nebo buffer místo oneshot wait, na první použít streaming a na další buffer
-#
-# vrep.simxGetObjectPosition(clientID, uavs[0], -1, vrep.simx_opmode_oneshot_wait)  # tímhle získám momentální polohu kvadrokoptéry, podle toho nasazuji další cíl
-
-
---
+++ testing.py
@@ -0,0 +1,6 @@
+var = 2
+print(var)
+arrVar = [2]
+print(arrVar)
+print(len(arrVar))
+print(*arrVar)
